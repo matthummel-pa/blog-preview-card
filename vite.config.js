3e6b38f31f187827c569b1c3a7ec550fe7e42680
Matt Hummel <matthummel@proton.me>
2025-03-09 11:32:17 -0400

    update
    change dist to docs for gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   root: './',
   publicDir: 'public',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     minify: true,
     sourcemap: true,
